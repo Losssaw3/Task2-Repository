3c361d163c81f911d1ea055512ae3d7a342e279d
losssaw3 <baylo.04@mail.ru>
2023-05-16 23:02:24 +0300

    tests

---
+++ SinglyLinkedList/SinglyLinkedListTest/SinglyLinkedListTest.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..//SingleList/List.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SinglyLinkedListTest
+{
+	TEST_CLASS(SinglyLinkedListTest)
+	{
+	public:
+		
+		
+	};
+}
